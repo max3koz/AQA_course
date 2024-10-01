bb638c8c0f7114805237abe3fc796dc373b6105b
Maksym Trykoz <max3koz@gmail.com>
2024-10-01 16:06:32 +0200

    Done: Updated the function delete_student_from_course and code refactoring

--- lesson_22/homework_l22.py
+++ lesson_22/homework_l22.py
@@ -4,9 +4,7 @@ import subprocess
 import traceback
 
 from logging import config
-from random import randint, sample
-from pony.orm import Required, Set, db_session, PrimaryKey, select, core, commit
-from pytest_check import equal
+from pony.orm import Set, db_session, PrimaryKey, select, core
 
 from lesson_22.BASE.pony_orm_db_client import PonyOrmDbClient
 from lesson_22.BASE.psql_client import PSQLClient
@@ -19,6 +17,7 @@ logging.getLogger('sampleLogger')
 subprocess.run("sudo systemctl start postgresql", shell=True)
 
 # The "postgresql" client to delete created tables finally
+# Please use your own data for DB "course_list"
 dbname = "course_list"
 user = "mtrykoz"
 password = "12345678"
@@ -33,6 +32,7 @@ client = PSQLClient(dbname=dbname,
 
 try:
     # The client to connect by PonyORM to course_list DB
+    # Please use your own data for DB "course_list"
     client_course_list = PonyOrmDbClient(provider='postgres',
                                          user='mtrykoz',
                                          password='12345678',
@@ -61,12 +61,12 @@ try:
                 action_2 = False
             else:
                 print("The exist course list:")
-                Course.select().show()
+                for course in Course.select():
+                    print(f"  - {course.course_name}")
                 new_course_list = input("Enter a COURSE for new student from the exist course list, "
                                         "separating with a comma: ").split(", ")
                 try:
                     Student(students_name=new_student_name, courses=get_selected_courses(new_course_list))
-                    print(get_selected_courses(new_course_list))
                 except core.CacheIndexError:
                     logging.error(f"Cannot create STUDENT: instance with \"{new_student_name}\" already exists!!!")
                     print("Try to enter other student name or choice the function to add new course to student!!!")
@@ -75,7 +75,7 @@ try:
     def add_new_course():
         action_1 = True
         while action_1:
-            new_course_name = input("Enter COURSE name (for finish enter \"q\"):")
+            new_course_name = input("Enter COURSE name (for finish enter \"q\"): ")
             if new_course_name == "q":
                 action_1 = False
             else:
@@ -109,32 +109,25 @@ try:
     def delete_student_from_course():
         deleted_student = input("Enter STUDENT name for the deleting: ")
         deleted_course = input("Enter COURSE for the deleting: ")
-        select_courses_for_student = select(student.courses
-                                            for student in Student if student.students_name == deleted_student)
-        update_course_list = []
-        for course in list(select_courses_for_student):
-            if course.course_name != deleted_course:
-                update_course_list.append(course.course_name)
-        select_student = select(student.students_name
-                                            for student in Student if student.students_name == deleted_student)
-        select_student.courses = get_selected_courses(update_course_list)
-        client_course_list.db.commit()
-        print(select_student.courses)
+        select_student = list(select(student for student in Student if student.students_name == deleted_student))[0]
+        course_for_delete = list(select(course for course in Course if course.course_name == deleted_course))[0]
+        select_student.courses.remove(course_for_delete)
 
+    # Create default test courses list to DB
     with db_session:
-        # Create default test courses list to DB
         courses = [Course(course_name=f"Course_{number}") for number in range(1, 6)]
 
-        while True:
-            print("Make your action choice:")
-            print("    enter \"1\" - add new COURSE to DB")
-            print("    enter \"2\" - add new STUDENT to course")
-            print("    enter \"3\" - delete STUDENT from COURSE")
-            print("    enter \"4\" - select STUDENT with courses from DB")
-            print("    enter \"5\" - select COURSE with students from DB")
-            print("    enter \"q\" - Quit from the application")
-            choice_action = input("Enter here: ")
+    while True:
+        print("Make your action choice:")
+        print("    enter \"1\" - add new COURSE to DB")
+        print("    enter \"2\" - add new STUDENT to course")
+        print("    enter \"3\" - delete STUDENT from COURSE")
+        print("    enter \"4\" - select STUDENT with courses from DB")
+        print("    enter \"5\" - select COURSE with students from DB")
+        print("    enter \"q\" - Quit from the application")
+        choice_action = input("Enter here: ")
 
+        with db_session:
             if choice_action == "1":
                 add_new_course()
             elif choice_action == "2":
@@ -150,17 +143,13 @@ try:
             else:
                 pass
 
-        Course.select().show()
-        print()
-        Student.select().show()
-
 except Exception as exception:
     print(traceback.format_exc())
 
 finally:
-    logging.info("Delete table \"course_student\", \"course\", \"student\".")
     client.execute_data_db("DROP TABLE course_student;")
     client.execute_data_db("DROP TABLE course;")
     client.execute_data_db("DROP TABLE student;")
+    logging.info("Delete table \"course_student\", \"course\", \"student\".")
 
     subprocess.run("sudo systemctl stop postgresql", shell=True)
