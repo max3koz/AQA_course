170a9c445cd801b75857793be3118fc388f4c075
Maksym Trykoz <max3koz@gmail.com>
2024-10-20 00:19:04 +0200

    Done: Added the task of the homework for lesson 28

---
+++ lesson_28/Pages/AbstractPage.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from selenium.webdriver.ie.webdriver import WebDriver
+
+from lesson_28.Web_elements.abstract_element import AbstractElement
+
+
+class AbstractPage(ABC):
+    _driver: WebDriver
+    _wrapped_element: AbstractElement
+
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+        self._wrapped_element = AbstractElement(self._driver)
